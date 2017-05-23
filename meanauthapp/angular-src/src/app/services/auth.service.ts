409abf2f9ffb4795a54ce9d39d57b1753205c623
Neo Dev <neodev31@gmail.com>
2017-05-23 00:33:17 -0400

    finished meanauth project.. commit before changes for deploying to heroku

--- meanauthapp/angular-src/src/app/services/auth.service.ts
+++ meanauthapp/angular-src/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 
 @Injectable()
@@ -21,4 +22,35 @@ export class AuthService {
     headers.append('Content-Type', 'application/json');
     return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers}).map(res => res.json());
   }
+
+  getProfile(){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', {headers: headers}).map(res => res.json());
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('token');
+    this.authToken = token;
+  }
+
+  storeUserData(token, user){
+    localStorage.setItem('token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  loggedIn(){
+    return tokenNotExpired();
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
+  }
+
 }
